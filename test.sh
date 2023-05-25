e3233b6e3f510b2f8c513d3e78ad5d092dd66349
Nat Quayle Nelson <natquaylenelson@gmail.com>
2023-05-25 15:55:37 -0600

    fix haxe_libraries handling for test project

--- test.sh
+++ test.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
 
-lix dev hollywoo-flixel .
-(cd src/test && lix run lime test neko -debug)
+target=${1:-cpp}
+
+cp -rf haxe_libraries src/test/
+(cd src/test && lix dev hollywoo-flixel ../../ && lix download && lix run lime test $target -debug)
